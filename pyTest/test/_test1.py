8f1411ad200f7173fc483bfbf4a3354ac902ea1c
hanxu <940274999@qq.com>
2018-06-25 14:42:30 +0800

    test1功能模块开发完毕，此分支结束

--- pyTest/test/_test1.py
+++ pyTest/test/_test1.py
@@ -9,9 +9,28 @@
 #  
 #---------------------------------------------------
 
-我是开发分支，
-开发呀。
-开发呀。
-开发呀。
-
-
+# 我是开发分支，
+# 开发呀。
+# 开发呀。
+# 开发呀。
+#
+# 再次开发呀，开发呀，不能提交喔40%
+# 再次开发呀，开发呀，不能提交喔50%
+# 呀，突然给我个bug了， 又不能提交，/wn ，那就只能先暂存一下那。git stash,保存当前进度，然后在master上开一个新的issue分支
+#     git stash
+#     git checkout master
+#     git checkout -b issue-2
+#         修复bug， bug是os.py 文件第10行的，多了最后一个字母a
+# bug,修复完毕。
+#          git commit -a -m "修复原因"     at branch of issue-2
+#     git checkout master
+#         合并issue分支到master， git merge --no-ff issue-2
+#         删除issue分支  git branch -d issue-2
+#  切换回原来的dev分支
+#    恢复暂存  git stash pop
+# 继续开发分支60%。
+# 开发呀
+# 开发呀
+# 开发完毕100%
+# 准备提交
+# git commit -a -m "分支开发完毕"
