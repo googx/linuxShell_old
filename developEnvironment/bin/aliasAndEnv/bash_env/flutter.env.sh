90c35b8e81307f12f2880ba54fb3431fe8949337
hanxu <940274999@qq.com>
2018-07-02 21:30:56 +0800

    新增加一个操作virtualbox的脚本，包含创建查看开启停止虚拟机的功能。

---
+++ developEnvironment/bin/aliasAndEnv/bash_env/flutter.env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+####################################################
+#
+# File Name: java.env.sh
+# Author: hx940929
+# AuthorSite: https://www.thesunboy.com/
+# Created Time: 2018年01月31日 星期三 14时33分31秒
+###-----------------说明-------------------------###
+#                                                   
+####################################################
+flutterhome=/usr/local/flutter/flutterSdk
+
+
+export PATH=$PATH:${flutterhome}/bin
